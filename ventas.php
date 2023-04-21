5bdf8b37828b83cdab43bb9dcebdb26af149ae37
julianmontero <julianmontero@me.com>
2023-04-21 16:17:56 -0300

    Actualización archivo ventas

--- ventas.php
+++ ventas.php
@@ -1,1 +1,10 @@
-echo "ventas";
+<?php
+       ini_set("display_errors","Off");
+       require "config/db.config.php";
+    
+        $conexion=mysqli_connect($db_host,$db_usuario,$db_clave,$db_nombre);
+ 
+        mysqli_close($conexion); 
+
+?>
+
